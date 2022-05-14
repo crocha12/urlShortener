fecda635f5db23b45d3d72f221d4243ece33ecce
Camilo <camilorocha11@gmail.com>
2022-05-13 22:46:42 -0300

    add nodemon and mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/url_shortener').then(() => console.log('db is connected')).catch(err => console.log(err))
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Hello world!' })
